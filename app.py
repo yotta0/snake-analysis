1cb4a6c401a04d4ffa8e7fb22e3e353ac32df2c2
Davi Castro <davicastroti@hotmail.com>
2021-08-20 02:39:10 -0300

    Created initial logic

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import urllib.request
+import requests as r
 
-url = 'url'
+params = "filetype"
+extension = "pdf"
+search_input = input('Search here')
+url = f'www.google.com/search?q={search_input}+{params}+{extension}'
 
 # Download the file from `url` and save it locally under `file_name`:
 with urllib.request.urlopen(url) as response, open('document.pdf', 'wb') as out_file:
